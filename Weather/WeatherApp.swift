525ac170e0244c433b294cbf8e5fef1e95f5b7fe
Thibaut Leroy <pro@thibautleroy.com>
2022-05-26 12:48:07 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Pro on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
